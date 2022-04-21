edd78a1ee3da2e9f0be69fab6a2f7fc69f3d28f7
altananay <altananay@outlook.com>
2022-04-21 16:01:25 +0300

    21.04.2022

--- src/pages/Egitimler.js
+++ src/pages/Egitimler.js
@@ -4,7 +4,6 @@ import { Container } from "semantic-ui-react";
 export default function Egitimler() {
   return (
     <div>
-      {document.body.style.background = ""}
       <Container text>Egitimler buraya...</Container>
     </div>
   );
